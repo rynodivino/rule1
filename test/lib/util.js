3cc330843c2f7a735670f31db0729ee61d2f31d6
rynodivino <rynodivino@gmail.com>
2014-01-05 11:15:58 -0800

    actually adding that test and travis yml

---
+++ test/lib/util.js
@@ -0,0 +1,9 @@
+var calculator  = require('../../lib/util.js'),
+    test = require('tap').test;
+
+
+test("Test roic calculation.", function(t) {
+    var result = calculator.roic(50, 100, 20);
+    t.equal(result, 160, "roic for da da da");
+    t.end();
+});
